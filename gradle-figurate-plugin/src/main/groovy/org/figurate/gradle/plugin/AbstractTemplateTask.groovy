bf94f6e31ec889133b62f01fb4a5dfb9bd9fbc4a
Ben Fortuna <bfortuna@odecee.com.au>
2015-05-03 00:24:13 +1000

    Use output directory annotation rather than manually create dirs

--- gradle-figurate-plugin/src/main/groovy/org/figurate/gradle/plugin/AbstractTemplateTask.groovy
+++ gradle-figurate-plugin/src/main/groovy/org/figurate/gradle/plugin/AbstractTemplateTask.groovy
@@ -2,8 +2,8 @@ package org.figurate.gradle.plugin
 
 import groovy.text.Template
 import org.gradle.api.DefaultTask
+import org.gradle.api.tasks.OutputDirectory
 import org.gradle.api.tasks.TaskAction
-import org.gradle.api.tasks.TaskOutputs
 
 /**
  * Created by fortuna on 10/05/14.
@@ -12,14 +12,11 @@ abstract class AbstractTemplateTask extends DefaultTask {
 
     static final String DEFAULT_CONFIG_DIR = 'conf'
 
-    def outputDir = "$project.buildDir/$DEFAULT_CONFIG_DIR"
+    @OutputDirectory
+    File outputDir = project.file("$project.buildDir/$DEFAULT_CONFIG_DIR")
 
     @TaskAction
     def createConfig() {
-        TaskOutputs taskOutputs = outputs.dir project.file(outputDir)
-        taskOutputs.files.each { outputDir ->
-            outputDir.mkdirs()
-        }
         new File(outputDir, getOutputFilename()).write(template.make(binding) as String)
     }
 
