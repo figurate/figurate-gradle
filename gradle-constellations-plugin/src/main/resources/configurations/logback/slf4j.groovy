43196f55f2124cfa7c6b973dcac85aca05b40dc6
Ben Fortuna <bfortuna@odecee.com.au>
2015-05-03 23:13:47 +1000

    Added required slf4j bundle

--- gradle-constellations-plugin/src/main/resources/configurations/logback/slf4j.groovy
+++ gradle-constellations-plugin/src/main/resources/configurations/logback/slf4j.groovy
@@ -1,2 +1,1 @@
-['ch.qos.logback:logback-core:1.1.1',
- 'ch.qos.logback:logback-classic:1.1.1']
+['org.slf4j:slf4j-api:1.7.12', 'ch.qos.logback:logback-core:1.1.3', 'ch.qos.logback:logback-classic:1.1.3']
