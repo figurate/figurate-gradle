485680e81cdbfb514846667ce345fc787a6ecfe1
Ben Fortuna <bfortuna@odecee.com.au>
2015-05-04 01:19:26 +1000

    Disabled integration tests

--- gradle-aem-plugin/src/test/groovy/org/figurate/gradle/plugin/aem/AemPluginTest.groovy
+++ gradle-aem-plugin/src/test/groovy/org/figurate/gradle/plugin/aem/AemPluginTest.groovy
@@ -9,6 +9,7 @@ import spock.lang.Specification
 /**
  * Created by fortuna on 27/02/15.
  */
+@Ignore('Don\'t run integration tests in CI')
 class AemPluginTest extends Specification {
 
     Project project
@@ -33,12 +34,31 @@ class AemPluginTest extends Specification {
 
     def 'verify unpack aem task'() {
         given: 'a configured project'
+        project.aem {
+            installDir = '/tmp/aem3'
+            installFile = '/Users/fortuna/Development/cq5/author/cq5-author-p4502.jar'
+        }
 
         when: 'the unpack aem task is executed'
         project.tasks.unpackAem.execute()
 
         then: 'the aem application is copied to the build directory'
         new File(project.buildDir, 'aem/crx-quickstart').exists()
+    }
+
+    @IgnoreRest
+    def 'verify copy aem license task'() {
+        given: 'a configured project'
+        project.aem {
+            installDir = '/tmp/aem3'
+            licenseDir = '/Users/fortuna/Development/cq5/author'
+        }
+
+        when: 'the copy aem license task is executed'
+        project.tasks.copyAemLicense.execute()
+
+        then: 'the aem license is copied to the install dir'
+        new File(project.aem.installDir, 'license.properties').exists()
     }
 
     @Ignore
@@ -52,12 +72,17 @@ class AemPluginTest extends Specification {
         new File(project.buildDir, 'aem/error.log').exists()
     }
 
-    @IgnoreRest
     def 'verify start aem task'() {
         given: 'a configured project'
-        project.aem.installDir = '/tmp'
+        project.aem {
+            installDir = '/tmp/aem3'
+            licenseDir = '/Users/fortuna/Development/cq5/author'
+            installFile = '/Users/fortuna/Development/cq5/author/cq5-author-p4502.jar'
+        }
 
         when: 'the start aem task is executed'
+        project.tasks.unpackAem.execute()
+        project.tasks.copyAemLicense.execute()
         project.tasks.startAem.execute()
 
         then: 'aem is started'
@@ -66,9 +91,10 @@ class AemPluginTest extends Specification {
 
     def 'verify stop aem task'() {
         given: 'a configured project'
-        project.aem.installDir = '/tmp'
+        project.aem.installDir = '/tmp/aem3'
 
         when: 'the stop aem task is executed'
+        project.tasks.unpackAem.execute()
         project.tasks.stopAem.execute()
 
         then: 'aem is stopped'
