ade83e68476cf040071ca82629172dc085b646de
Ben Fortuna <bfortuna@odecee.com.au>
2015-05-08 01:32:49 +1000

    Added configuration admin to default felix constellation

--- gradle-constellations-plugin/src/main/resources/configurations/felix/felix.groovy
+++ gradle-constellations-plugin/src/main/resources/configurations/felix/felix.groovy
@@ -2,4 +2,4 @@
  'org.apache.felix:org.apache.felix.http.bundle:2.2.2',
  'org.apache.felix:org.apache.felix.log:1.0.1',
  'org.apache.felix:org.apache.felix.scr:1.8.2',
- 'org.osgi:org.osgi.compendium:5.0.0']
+ 'org.osgi:org.osgi.compendium:5.0.0', 'org.apache.felix:org.apache.felix.configadmin:1.8.0']
