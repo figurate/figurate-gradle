b2d9e3f0b9c7236049ca2e39d4452c30b4b0ccf7
Ben Fortuna <bfortuna@odecee.com.au>
2015-05-09 00:28:44 +1000

    Replaced jtidy with jsoup

--- gradle-constellations-plugin/src/main/resources/configurations/newsagent/rss.groovy
+++ gradle-constellations-plugin/src/main/resources/configurations/newsagent/rss.groovy
@@ -1,4 +1,4 @@
 ['org.mnode.newsagent:newsagent:master-SNAPSHOT', 'org.slf4j:slf4j-api:1.7.10', 'org.codehaus.groovy:groovy-all:2.4.3',
  'net.sf.ehcache:ehcache:2.10.0',
  'org.apache.jackrabbit:jackrabbit-jcr-commons:2.10.0', 'javax.jcr:jcr:2.0',
- 'org.bouncycastle:bcprov-jdk16:1.46']
+ 'org.bouncycastle:bcprov-jdk16:1.46', 'org.jsoup:jsoup:1.8.2']
