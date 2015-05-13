6c9f8f4ea8a1ecabcf12860580c37ce2660968e9
Ben Fortuna <bfortuna@odecee.com.au>
2015-05-14 01:20:17 +1000

    Upgrade amdatu rest and its dependencies to latest release

--- gradle-constellations-plugin/src/main/resources/configurations/amdatu/rest.groovy
+++ gradle-constellations-plugin/src/main/resources/configurations/amdatu/rest.groovy
@@ -3,7 +3,4 @@
  'org.amdatu.web.rest.doc.swagger:org.amdatu.web.rest.doc.swagger:1.0.9',
  'org.amdatu.web.rest.doc.swagger.ui:org.amdatu.web.rest.doc.swagger.ui:1.0.7',
  'org.amdatu.web.rest.jaxrs:org.amdatu.web.rest.jaxrs:1.0.8',
- 'org.amdatu.web.rest.wink:org.amdatu.web.rest.wink:2.0.3',
- 'com.fasterxml.jackson.jaxrs:jackson-jaxrs-json-provider:2.5.3', 'com.fasterxml.jackson.core:jackson-annotations:2.5.3',
- 'com.fasterxml.jackson.core:jackson-core:2.5.3', 'com.fasterxml.jackson.core:jackson-databind:2.5.3',
- 'com.fasterxml.jackson.jaxrs:jackson-jaxrs-base:2.5.3']
+ 'org.amdatu.web.rest.wink:org.amdatu.web.rest.wink:2.0.3']
