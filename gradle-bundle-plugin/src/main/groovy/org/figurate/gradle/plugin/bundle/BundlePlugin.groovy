eed63d0035284ad62c91746c7bd568885ad03c98
Ben Fortuna <bfortuna@odecee.com.au>
2016-04-28 17:19:04 +1000

    Fix syntax of required output directory

--- gradle-bundle-plugin/src/main/groovy/org/figurate/gradle/plugin/bundle/BundlePlugin.groovy
+++ gradle-bundle-plugin/src/main/groovy/org/figurate/gradle/plugin/bundle/BundlePlugin.groovy
@@ -28,12 +28,15 @@ class BundlePlugin implements Plugin<Project> {
             ant.properties.src = "$project.buildDir/classes/main"
             ant.properties.classes = "$project.buildDir/classes/main"
 
-            task('genscr', dependsOn: [compileJava, compileGroovy]) << {
+            task('genscr', dependsOn: [compileJava, compileGroovy]) {
                 // genscr requires the src dir to exist, using output to create as needed..
                 outputs.dir ant.properties.classes
-                ant.taskdef(resource: 'scrtask.properties', classpath: sourceSets.main.compileClasspath.asPath)
-                ant.scr(srcdir: ant.properties.src, destdir: ant.properties.classes, scanClasses: true,
+
+                doLast {
+                    ant.taskdef(resource: 'scrtask.properties', classpath: sourceSets.main.compileClasspath.asPath)
+                    ant.scr(srcdir: ant.properties.src, destdir: ant.properties.classes, scanClasses: true,
                         classpath: sourceSets.main.compileClasspath.asPath)
+                }
             }
             jar.dependsOn(genscr)
 
