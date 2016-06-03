7e494325b2c80df519f6b3e8b9d1ab8a9995c9a4
Ben Fortuna <bfortuna@odecee.com.au>
2016-06-03 16:15:59 +1000

    Added wait plugin

---
+++ gradle-wait-plugin/src/main/groovy/org/figurate/gradle/plugin/waitfor/WaitForPlugin.groovy
@@ -0,0 +1,11 @@
+package org.figurate.gradle.plugin.waitfor
+
+import org.gradle.api.Plugin
+import org.gradle.api.Project
+
+class WaitForPlugin implements Plugin<Project> {
+    @Override
+    void apply(Project project) {
+
+    }
+}
